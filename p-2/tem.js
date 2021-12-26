f91cf959533ef162993f2f371aa9277697ea29a3
Robiul Hasan <muhammadrobiul502@gmail.com>
2021-12-26 22:01:16 +0600

    html

--- p-2/tem.js
+++ p-2/tem.js
@@ -1,13 +1,1 @@
 'use strict';
-
-// let input = prompt('enter your name');
-
-// let arr = [...input];
-
-// console.log(arr.length);
-
-console.log('r3t45i5', ...'r3t45i5');
-
-let arr = [...'r3t45i5'];
-
-console.log(arr.length);
